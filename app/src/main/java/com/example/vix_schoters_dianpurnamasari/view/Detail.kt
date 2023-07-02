0e8e36197da7084d80a38ae6b8438aaa0214c090
dianprnms <purnamasaridian010@gmail.com>
2023-07-02 22:30:57 +0700

    Detail News

--- app/src/main/java/com/example/vix_schoters_dianpurnamasari/view/Detail.kt
+++ app/src/main/java/com/example/vix_schoters_dianpurnamasari/view/Detail.kt
@@ -9,7 +9,6 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.example.vix_schoters_dianpurnamasari.databinding.FragmentDetailBinding
-import com.example.vix_schoters_dianpurnamasari.databinding.FragmentSourceBinding
 import com.example.vix_schoters_dianpurnamasari.model.Article
 import com.example.vix_schoters_dianpurnamasari.viewmodel.DetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,6 +17,20 @@ import dagger.hilt.android.AndroidEntryPoint
 class Detail : Fragment() {
     private lateinit var binding: FragmentDetailBinding
     private lateinit var detailViewModel: DetailViewModel
+    private var articleId: String? = null
+    private var judul: String? = null
+    private var gambar: String? = null
+    private var deskripsi: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            articleId = it.getString(ARG_ARTICLE_ID)
+            judul = it.getString("judul")
+            gambar = it.getString("gambar")
+            deskripsi = it.getString("deskripsi")
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,16 +46,28 @@ class Detail : Fragment() {
 
         detailViewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
 
-        val articleId = arguments?.getString(ARG_ARTICLE_ID)
-        if (articleId != null) {
-            detailViewModel.loadArticle(articleId)
+        articleId?.let { id ->
+            detailViewModel.loadArticle(id)
         }
 
         detailViewModel.article.observe(viewLifecycleOwner, Observer { article ->
-            if (article != null) {
-                bindData(article)
-            }
+            article?.let { bindData(it) }
         })
+
+        judul?.let { judul ->
+            binding.titleTextView.text = judul
+        }
+
+        gambar?.let { gambar ->
+            Glide.with(requireContext())
+                .load(gambar)
+                .into(binding.imageView)
+        }
+
+        deskripsi?.let { deskripsi ->
+            binding.descriptionTextView.text = deskripsi
+        }
+
     }
 
     private fun bindData(article: Article) {
@@ -56,10 +81,14 @@ class Detail : Fragment() {
     companion object {
         private const val ARG_ARTICLE_ID = "articleId"
 
-        fun newInstance(articleId: String): Detail {
+        fun newInstance(articleId: String, judul: String, gambar: String, deskripsi: String): Detail {
             val fragment = Detail()
-            val args = Bundle()
-            args.putString(ARG_ARTICLE_ID, articleId)
+            val args = Bundle().apply {
+                putString(ARG_ARTICLE_ID, articleId)
+                putString("judul", judul)
+                putString("gambar", gambar)
+                putString("deskripsi", deskripsi)
+            }
             fragment.arguments = args
             return fragment
         }
--- app/src/main/java/com/example/vix_schoters_dianpurnamasari/view/adapter/SourceAdapter.kt
+++ app/src/main/java/com/example/vix_schoters_dianpurnamasari/view/adapter/SourceAdapter.kt
@@ -1,14 +1,17 @@
 package com.example.vix_schoters_dianpurnamasari.view.adapter
 
+import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.vix_schoters_dianpurnamasari.R
 import com.example.vix_schoters_dianpurnamasari.databinding.ItemSourceBinding
 import com.example.vix_schoters_dianpurnamasari.model.Article
 
-class SourceAdapter(private var listDataSource: List<Article>) : RecyclerView.Adapter<SourceAdapter.ViewHolder>() {
-
+class SourceAdapter(private var listDataSource: List<Article>) :
+    RecyclerView.Adapter<SourceAdapter.ViewHolder>() {
 
     fun setData(dataSource: List<Article>) {
         listDataSource = dataSource
@@ -16,15 +19,23 @@ class SourceAdapter(private var listDataSource: List<Article>) : RecyclerView.Ad
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding = ItemSourceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        val binding =
+            ItemSourceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val dataSource = listDataSource[position]
         holder.bind(dataSource)
-        holder.binding.itemSource.setOnClickListener{
+        holder.binding.itemSource.setOnClickListener { view ->
+            val bundle = Bundle().apply {
+                putString("judul", dataSource.title)
+                putString("gambar", dataSource.urlToImage)
+                putString("deskripsi", dataSource.description)
 
+            }
+            view.findNavController()
+                .navigate(R.id.action_source_to_detail, bundle)
         }
     }
 
@@ -32,13 +43,13 @@ class SourceAdapter(private var listDataSource: List<Article>) : RecyclerView.Ad
         return listDataSource.size
     }
 
-    inner class ViewHolder(val binding: ItemSourceBinding) : RecyclerView.ViewHolder(binding.root) {
+    inner class ViewHolder(val binding: ItemSourceBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun bind(dataSource: Article) {
             binding.nameSource.text = dataSource.title
             Glide.with(binding.imageView.context)
                 .load(dataSource.urlToImage)
                 .into(binding.imageView)
-
         }
     }
 }
